ee10657319e12c0bc754400d7c4096244d69ee57
offfahad <mughalfahad544@gmail.com>
2023-09-22 19:30:03 +0500

    restaurants-list-added

--- lib/widgets/nearby_restaurant.dart
+++ lib/widgets/nearby_restaurant.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/data/data.dart';
+import 'package:food_delivery/models/restaurant.dart';
 
 class NearbyRestaurant extends StatelessWidget {
   const NearbyRestaurant({super.key});
 
-  _buildRestaurants(){
-    
+  _buildRestaurants() {
+    List<Widget> restaurantList = [];
+    restaurants.forEach((Restaurant restaurants) {
+      restaurantList.add(Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10.0,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(
+            width: 1.0,
+            color: Colors.grey,
+          ),
+        ),
+        child: Row(children: [
+          Image(
+            width: 150.0,
+            height: 150.0,
+            image: AssetImage(restaurants.imageUrl ?? ''),
+            fit: BoxFit.cover,
+          ),
+        ]),
+      ));
+    });
+    return Column(
+      children: restaurantList,
+    );
   }
 
   @override
@@ -15,7 +44,7 @@ class NearbyRestaurant extends StatelessWidget {
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: Text(
-            'Nearby Restaurnat',
+            'Nearby Restaurnats',
             style: TextStyle(
               fontSize: 24.0,
               fontWeight: FontWeight.w600,
