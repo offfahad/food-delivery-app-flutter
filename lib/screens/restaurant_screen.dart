19325ac3ec746c2c7b5c2eb52fafec025699667c
offfahad <mughalfahad544@gmail.com>
2023-09-23 13:23:11 +0500

    navigation-buttons-added

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/models/restaurant.dart';
 
 class RestaurantScreen extends StatefulWidget {
-  const RestaurantScreen({this.restaurant,super.key});
+  const RestaurantScreen({this.restaurant, super.key});
 
   final Restaurant? restaurant;
 
@@ -14,7 +14,39 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New Screen'),),
+      body: Column(children: [
+        Stack(
+          children: [
+            Image(
+              height: 220.0,
+              width: MediaQuery.of(context).size.width,
+              image: AssetImage(widget.restaurant!.imageUrl ?? ''),
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(Icons.arrow_back_ios),
+                    color: Colors.white,
+                    iconSize: 30.0,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite),
+                    color: Theme.of(context).primaryColor,
+                    iconSize: 35.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
